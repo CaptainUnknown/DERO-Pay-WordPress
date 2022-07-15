a7b774fcc9832804768f5d40489cc41e29b0ad7f
Captain Unknown <captainunknown7@gmail.com>
2022-07-15 21:53:15 +0500

    Added WPP Index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ class DERORPC {
     }
 
     function adminAssets() {
-        wp_enqueue_script('dero-payment-gateway', plugin_dir_url(__FILE__). 'test.js', array('wp-blocks', 'wp-element'));
+        wp_enqueue_script('dero-payment-gateway', plugin_dir_url(__FILE__). 'build/index.js', array('wp-blocks', 'wp-element'));
     }
 }
 
