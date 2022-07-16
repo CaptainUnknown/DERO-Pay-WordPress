c45b547aba6f7cd4120b8b5231dc3e8a1a3ffbc7
Captain Unknown <captainunknown7@gmail.com>
2022-07-16 15:15:03 +0500

    ➕ Added USD Price Conversion to DERO

--- src/index.js
+++ src/index.js
@@ -4,28 +4,61 @@ wp.blocks.registerBlockType('derorpc/gateway', {
     category: 'common',
     attribute: {
         SCID: {type: 'string'},
-        amount: {type: 'integer'},
+        USDamount: {type: 'integer'},
+        APIKey: {type: 'string'},
     },
     edit: (props) => {
         const updateSCID = (event) => {
             props.setAttributes({SCID: event.target.value});
         }
         const updateAmount = (event) => {
-            props.setAttributes({amount: event.target.value});
+            props.setAttributes({USDamount: event.target.value});
+        }
+        const updateAPIKey = (event) => {
+            props.setAttributes({APIKey: event.target.value});
+        }
+        const USDtoDERO = (event) => {
+            let myAPIKey = props.attributes.APIKey;
+            
+            const packet = {
+                "currency": "USD",
+                "code": "DERO"
+            }
+
+            const rawResponse = await fetch('https://api.livecoinwatch.com/coins/single', {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                  'x-api-key': myAPIKey
+                },
+                body: JSON.stringify(packet)
+            })
+            .catch(error => {alert(error)});
+            const response = await rawResponse.json();
+
+            let currentRate = response.rate;
+
+            DEROamount = USDamount * (1/currentRate);  //   Converts USD Amount to DERO Amount based upon current rate
+            DEROamount = DEROamount.toFixed(5);       //    Uses 5 Atomic Unit Precision for DERO Amount
+            
+            console.log(content);
         }
         return (
             <div>
                 <h3>DERO Payment Gateway Configuration</h3>
-                <input type='text' id='SCID' placeholder='Smart Contract Address' onChange={updateSCID}/>
-                <input type='text' id='amount' placeholder='Amount in DERO' onChange={updateAmount}/>
-                {/* /* Add a conversion function from Dollar value to current DERO Value */}
+                <input type='text' id='SCID' value={props.attributes.SCID} placeholder='Smart Contract Address' onChange={updateSCID}/>
+                <input type='number' id='amount' value={props.attributes.USDamount} placeholder='Amount in DERO' onChange={updateAmount}/>
+                <input type='number' id='APIKey' value={props.attributes.APIKey} placeholder='livecoinwatch API Key (Visit: livecoinwatch.com)' onChange={updateAPIKey}/>
             </div>
         )
     },
     save: (props) => {
         return (
             <div>
-                <p>{props.attributes.amount} DERO transfered to {props.attributes.SCID} SCID</p>
+                <h3>DERO Payment Gateway</h3>
+                <p>Smart Contract Address: {props.attributes.SCID}</p>
+                <p>Amount in USD: {props.attributes.USDamount}</p>
             </div>
         )
     }
