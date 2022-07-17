ebd0a4762c5054207777a88a49cab293fd4132e0
Captain Unknown <captainunknown7@gmail.com>
2022-07-17 17:17:46 +0500

    🔧 Changed Place Holders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles.css"
+
 wp.blocks.registerBlockType('derorpc/gateway', {
     title: 'DERO Payment Gateway',
     icon: 'money-alt',
@@ -17,19 +19,19 @@ wp.blocks.registerBlockType('derorpc/gateway', {
         const updateAPIKey = (event) => {
             props.setAttributes({APIKey: event.target.value});
         }
-        const USDtoDERO = async(event) => {
+        const USDtoDERO = async(event) => { //Move to save block
             const packet = {
                 "currency": "USD",
                 "code": "DERO"
             }
 
-            let myAPIKey = 'c8573f42-e797-43e9-811d-07effb255ad8'; //c8573f42-e797-43e9-811d-07effb255ad8 Hard Coded For DEBUGGING
+            let myAPIKey = 'c8573f42-e797-43e9-811d-07effb255ad8'; //Hard Coded For DEBUGGING
 
             if(props.attributes.APIKey == '') {
                 //Default API Key
                 myAPIKey = 'c8573f42-e797-43e9-811d-07effb255ad8';
             }
-            else{
+            else {
                 myAPIKey = props.attributes.APIKey;
             }
 
@@ -42,12 +44,13 @@ wp.blocks.registerBlockType('derorpc/gateway', {
                 },
                 body: JSON.stringify(packet)
             }).catch(error => {alert(error)});
+
             const content = await rawResponse.json();
             let currentRate = content.rate;
             
-            let DEROamount = 99.999 * (1/currentRate);
-            DEROamount = DEROamount.toFixed(5);         //   Converts USD Amount to DERO Amount based upon current rate
-            DEROamount = parseFloat(DEROamount);       //    Uses 5 Atomic Unit Precision for DERO Amount
+            let DEROamount = USDamount * (1/currentRate);   //   Converts USD Amount to DERO Amount based upon current rate
+            DEROamount = DEROamount.toFixed(5);            //    Uses 5 Atomic Unit Precision for DERO Amount
+            DEROamount = parseFloat(DEROamount);
 
             console.log(content.rate);
             console.log(content);
@@ -55,10 +58,13 @@ wp.blocks.registerBlockType('derorpc/gateway', {
         }
         return (
             <div>
-                <h3>DERO Payment Gateway Configuration</h3>
-                <input type='text' id='SCID' value={props.attributes.SCID} placeholder='Smart Contract Address' onChange={updateSCID}/>
-                <input type='number' id='amount' value={props.attributes.USDamount} placeholder='Amount in DERO' onChange={updateAmount}/>
-                <input type='number' id='APIKey' value={props.attributes.APIKey} placeholder='livecoinwatch API Key (Visit: livecoinwatch.com)' onChange={updateAPIKey}/>
+                <h3>🔏 DERO Payment Gateway 🎛️ Configuration 🔧</h3>
+                <p>Smart Contract ID 📃:</p>
+                <input type='text' id='SCID' value={props.attributes.SCID} placeholder='0x______' onChange={updateSCID}/><br/><br/>
+                <p>USD Amount 💲:</p>
+                <input type='number' id='amount' value={props.attributes.USDamount} placeholder='$100' onChange={updateAmount}/><br/><br/>
+                <p>livecoinwatch.com 🪙 API Key 🗝️ (Optional):</p>
+                <input type='text' id='APIKey' value={props.attributes.APIKey} placeholder='XXXX-XXXX' onChange={updateAPIKey}/><br/><br/>
             </div>
         )
     },
