6e79bc19437448faaaf4b4ec90510bbf7d062172
Captain Unknown <captainunknown7@gmail.com>
2022-07-22 14:59:48 +0500

    ➕ Added Two Contract Parameters

--- backend/payment-gateway.js
+++ backend/payment-gateway.js
@@ -2,8 +2,11 @@ import "./payment-gateway.scss"
 import { registerBlockType } from "@wordpress/blocks"
 
 const EditComponent = (props) => {
-    const updateSCID = (event) => {
-        props.setAttributes({SCID: event.target.value});
+    const updateDSCID = (event) => {
+        props.setAttributes({DSCID: event.target.value});
+    }
+    const updateTSCID = (event) => {
+        props.setAttributes({TSCID: event.target.value});
     }
     const updateAmount = (event) => {
         props.setAttributes({USDamount: event.target.value});
@@ -11,15 +14,22 @@ const EditComponent = (props) => {
     const updateAPIKey = (event) => {
         props.setAttributes({APIKey: event.target.value});
     }
+    const updateOwnerWallet = (event) => {
+        props.setAttributes({ownerWallet: event.target.value});
+    }
     return (
         <div>
-            <h3>🔏 DERO Payment Gateway 🎛️ Configuration 🔧</h3>
-            <p>Smart Contract ID 📃:</p>
-            <input type='text' id='SCID' value={props.attributes.SCID} placeholder='0x______' onChange={updateSCID}/><br/>
+            <h3>🔏 DERO Payment Gateway 🎛️ Configuration! 🔧</h3>
+            <p>Token 🪙 Payment Smart Contract 🆔 📃:</p>
+            <input type='text' id='TSCID' value={props.attributes.TSCID} placeholder='0x______' onChange={updateTSCID}/><br/>
+            <p>DERO 💱 Payment Smart Contract 🆔 📃:</p>
+            <input type='text' id='DSCID' value={props.attributes.DSCID} placeholder='0x______' onChange={updateDSCID}/><br/>
             <p>USD Amount 💲:</p>
             <input type='number' id='amount' value={props.attributes.USDamount} placeholder='$100' onChange={updateAmount}/><br/>
             <p>livecoinwatch.com 🪙 API Key 🗝️ (Optional):</p>
             <input type='text' id='APIKey' value={props.attributes.APIKey} placeholder='XXXX-XXXX' onChange={updateAPIKey}/><br/>
+            <p>Owner Wallet 📇:</p>
+            <input type='text' id='ownerWallet' value={props.attributes.ownerWallet} placeholder='dero1qyXXXX' onChange={updateOwnerWallet}/><br/>
         </div>
     )
 }
@@ -29,9 +39,11 @@ registerBlockType("dero/payment-gateway", {
     icon: 'money-alt',
     category: "common",
     attributes: {
-        SCID: {type: 'string'},
+        TSCID: {type: 'string'},
+        DSCID: {type: 'string'},
         USDamount: {type: 'integer'},
         APIKey: {type: 'string'},
+        ownerWallet: {type: 'string'}
     },
     edit: EditComponent,
     save: function() {
