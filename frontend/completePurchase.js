363b3c1ac8c416d08e82bafcb6072c17928da549
Captain Unknown <captainunknown7@gmail.com>
2022-09-04 16:51:21 +0500

    ➕ Added Custom EndPoint implementation

--- frontend/completePurchase.js
+++ frontend/completePurchase.js
@@ -6,27 +6,99 @@ const handleError = (error) => {
     }));
 }
 
-export const completePurchase = async(courseID, userID) => {
-    let data = {"user_ids": [ userID ]};
-    let packet = JSON.stringify(data);
-    console.log(purchaseData.nonce);
-    const response = await (fetch(
-        `https://templisaquaria.com/wp-json/ldlms/v1/sfwd-courses/${courseID}/users`,
-        {
+export const completePurchase = async(options) => {
+    if (action == 'learnDash') {
+        //send fetch to backend
+        let data = { "user_ids": [userID] };
+        let packet = JSON.stringify(data);
+        console.log(purchaseData.nonce);
+        const response = await (fetch(options.shopify.url + options.shopify.courseID, {
             body: packet,
             headers: {
-                'Content-Type': "application/json",
-                'X-WP-Nonce': purchaseData.nonce
+                'Content-Type': "application/json"
             },
             method: "POST"
         })
         .catch((error) => {
             handleError(error);
         }));
-    
-    if (response.ok) {
-        return response.json();
-    } else {
-        return Promise.reject(response);
+
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }
+    else if (action == 'customEP'){
+        const myHeaders =  JSON.parse(options.customEP.headers);
+        const myURL = `${JSON.parse(options.customEP.url)}`;
+        if(options.customEP.method == 'GET'){
+            const response = await (fetch(myURL, {
+                headers: myHeaders,
+                method: "GET"
+            })
+            .catch((error) => {
+                handleError(error);
+            }));
+
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        }
+        else if(options.customEP.method == 'POST'){
+            const response = await (fetch(myURL, {
+                body: options.customEP.body,
+                headers: myHeaders,
+                method: "POST"
+            })
+            .catch((error) => {
+                handleError(error);
+            }));
+            
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        }
+        else if(options.customEP.method == 'PUT'){
+            const response = await (fetch(myURL, {
+                body: options.customEP.body,
+                headers: myHeaders,
+                method: "PUT"
+            })
+            .catch((error) => {
+                handleError(error);
+            }));
+            
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        }
+        else if(options.customEP.method == 'DELETE'){
+            const response = await (fetch(myURL, {
+                headers: myHeaders,
+                method: "DELETE"
+            })
+            .catch((error) => {
+                handleError(error);
+            }));
+            
+            if (response.ok) {
+                return response.json();
+            } else {
+                return Promise.reject(response);
+            }
+        }
+    }
+    else if (action == 'shopify'){
+
+    }
+    else {
+        alert('❌ No action specified');
     }
 }
